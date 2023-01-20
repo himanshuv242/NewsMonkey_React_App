0969f7a6dd658f1479ada0a962a6de01a5bb6741
Himanshu Verma <himanshuv242cp@gmail.com>
2023-01-21 02:49:42 +0530

    Added author, time and source

--- src/components/News.js
+++ src/components/News.js
@@ -83,7 +83,7 @@ export class News extends Component {
         {!this.state.loading && this.state.articles.map((element)=>{
             return(
                 <div key={element.url} className="col-md-4">
-            <NewsItem title={element.title? element.title.slice(0,45):""} description={element.description?element.description.slice(0,88): ""} imageUrl={element.urlToImage?element.urlToImage:"https://image.cnbcfm.com/api/v1/image/107180827-1674115488690-gettyimages-1246344394-AA_19012023_1029871.jpeg?v=1674116512&w=1920&h=1080"} newsUrl={element.url}/>
+            <NewsItem title={element.title? element.title.slice(0,45):""} description={element.description?element.description.slice(0,88): ""} imageUrl={element.urlToImage?element.urlToImage:"https://image.cnbcfm.com/api/v1/image/107180827-1674115488690-gettyimages-1246344394-AA_19012023_1029871.jpeg?v=1674116512&w=1920&h=1080"} newsUrl={element.url} author={element.author?element.author:"unknown"} date={element.publishedAt} source={element.source.name}/>
             </div>
             )
         })}
